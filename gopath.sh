8f5a91714d411b11e6b6f2fa7d3296d8c00b9e52
Eddy Reyes <ereyes01@users.noreply.github.com>
2016-02-06 18:56:19 -0600

    When running gopath multiple times, clobber the previous $GOPATH/bin in the $PATH so that it doesn't grow arbitrarily long and keep old GOPATHs in there.

--- gopath.sh
+++ gopath.sh
@@ -3,8 +3,11 @@
 gopath()
 {
     dir=$(readlink -m $1)
-
     [[ -d "$dir" ]] || return 1
+
+    if [[ -n "$GOPATH" ]]; then
+        PATH=${PATH##$GOPATH/bin:}
+    fi
 
     export GOPATH=$dir
     export PATH=$GOPATH/bin:$PATH
